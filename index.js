5901a6a0ae2cb60021fa37da3a766e9f1a71f51b
Mathieu Dutour <mat.dutour@gmail.com>
2019-03-10 11:54:37 -0400

    update @octokit/rest and use @octokit/app

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const jwt = require('jsonwebtoken')
-const GitHubApi = require('@octokit/rest')
+const App = require('@octokit/app')
+const Octokit = require('@octokit/rest')
 
 module.exports = function ({id, cert, debug = false}) {
+  const app = new App({ id, privateKey: cert, debug })
+  
   function asApp () {
-    const github = new GitHubApi({debug})
-    github.authenticate({type: 'app', token: generateJwt(id, cert)})
-    // Return a promise to keep API consistent
-    return Promise.resolve(github)
+    return Promise.resolve(app)
+  }
+  
+  // https://developer.github.com/early-access/integrations/authentication/#as-an-installation
+  function createToken (installationId) {
+    return app.getInstallationAccessToken({
+      installation_id: installationId
+    })
   }
 
   // Authenticate as the given installation
   function asInstallation (installationId) {
-    return createToken(installationId).then(res => {
-      const github = new GitHubApi({debug})
-      github.authenticate({type: 'token', token: res.data.token})
-      return github
+    const octokit = new Octokit({
+      auth() {
+        return createToken(installationId)
+          .then(installationAccessToken => 'token ' + installationAccessToken)
+      },
     })
-  }
 
-  // https://developer.github.com/early-access/integrations/authentication/#as-an-installation
-  function createToken (installationId) {
-    return asApp().then(github => {
-      return github.apps.createInstallationToken({
-        installation_id: installationId
+    return createToken(installationId).then(installationAccessToken => {
+      const octokit = new Octokit({
+        auth: 'token ' + installationAccessToken,
       })
+      
+      // expose installationa access token as it can be useful
+      if (!octokit.auth) {
+        octokit.auth = {}
+      }
+      octokit.auth.token = installationAccessToken
+      
+      return octokit
     })
-  }
-
-  // Internal - no need to exose this right now
-  function generateJwt (id, cert) {
-    const payload = {
-      iat: Math.floor(new Date() / 1000), // Issued at time
-      exp: Math.floor(new Date() / 1000) + 60, // JWT expiration time
-      iss: id // Integration's GitHub id
-    }
-
-    // Sign with RSA SHA256
-    return jwt.sign(payload, cert, {algorithm: 'RS256'})
   }
 
   return {asApp, asInstallation, createToken}
